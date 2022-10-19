69754f25c1690cffb6686a5006e3bb88ccfd11fc
sotatek-HarryPhi <e.hai.phi@wesang.com>
2022-10-19 22:22:20 +0700

    13. Using the validator

--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -1,7 +1,8 @@
 import { Response, Router } from 'express';
 import { Request } from 'express-validator/src/base';
 import { TasksController } from './task.controller';
-
+import { createValidator } from './tasks.validator';
+import { validationResult } from 'express-validator';
 export const taskRouter: Router = Router();
 
 taskRouter.get(
@@ -10,5 +11,21 @@ taskRouter.get(
     const taskController = new TasksController();
     const allTasks = await taskController.getAll();
     res.json(allTasks);
+  },
+);
+
+taskRouter.post(
+  '/tasks',
+  createValidator,
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //@ts-ignore
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ errors: errors.array() });
+    }
   },
 );
